0f45193744763f5e36a69a7f6f04e0b98adca763
Margarita Leonova <margarita.leonova@outlook.com>
2024-06-01 14:57:57 +0300

    Backend: Create login endpoint

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
+from app.models import Agent
+from app import db
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/')
-def get_data():
-    data = {'message': 'Hello from Auth page!'}
-    return jsonify(data)
+# endpoint for agent login
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    agent = Agent.query.filter_by(email=email).first()
+
+    if agent and agent.check_password(password):
+        return jsonify({'message': 'Login successful', 'agent_id': agent.id}), 200
+    else:
+        return jsonify({'message': 'Invalid email or password'}), 401
