9d9a880cd3bc674084c46dba6e67a330098e616a
Margarita Leonova <margarita.leonova@outlook.com>
2024-06-02 20:53:17 +0300

    Backend: Updated config.py file

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://mleonova:csa1234@localhost:5432/customer_service_db'
